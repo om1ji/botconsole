c0a37747ff812e9a7b38a4ea4a2b45aa19779cbb
Om1ji <om1ji.music@gmail.com>
2021-07-14 21:01:34 +0500

    Добавлен метод getMe. Улучшение вёрстки

--- main.py
+++ main.py
@@ -1,28 +1,9 @@
-import requests
-import os
-import telebot
 from tkinter import *
+from blocks import *
 
-root = Tk()
+root = Tk(screenName='TGBot Console')
+root.title('TGBot Console')
+root.geometry('300x150')
 
-class Blocks:
-    def __init__(self, window,  func):
-        self.ent = Entry(window, width=20)
-        self.but = Button(window, text="Токен")
-        self.lab = Label(window, width=20, bg='black', fg='white')
-        self.but['command'] = getattr(self, func)
-        self.ent.pack()
-        self.but.pack()
-        self.lab.pack()
-
-    def init_token(self):
-        self.lab['text'] = self.ent.get()
-        token = self.lab['text']
-
-token_block = Blocks(root, 'init_token')
-
-
-if __name__=='__main__':
-    root.mainloop()
-
-#BOT = telebot.TeleBot(TOKEN)
+token_block = GetMe(root, 'GetMe')
+root.mainloop()
